aa625efffc23212b093109e90558de51bb0d9369
Péter Sebők <sebok.peter97@gmail.com>
2018-10-11 13:03:24 +0200

    Dynamic page v0.01

---
+++ Product page/js/products.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        url: "https://ipcsmmd-webshop-group16.azurewebsites.net/api/beers?CurrentPage=1&ItemsPerPage=3&isAscending=true&SearchField=id",
+        dataType: "json",
+        success: function (beers) {
+            listBeers(beers);
+        },
+        error: function(request, message, error){
+            handleException(request, message, error);
+        }
+    });
+});
+
+    $.each(beers, function (index, beer) { 
+        console.log('Beer name:' + beer.name);
+        console.log('Beer brand: ' + beer.brand);
+        console.log('Beer type: ' + beer.type);
+        console.log('Beer price: ' + beer.price);
+        console.log('Beer percentage: ' + beer.percentage);
+        buildDescriptionPage(beer);
+    });
+}
+
+function buildDescriptionPage(beer){
+    var s = ' <div id="product"> ' + 
+    '<div id="producttitle">' + beer.name + '</div>' +
+    '<div id="productimage"> <img src="' + beer.imageURL + '" alt="css/carlsbergcan.png"></div>' +
+    '<div id="price">Price: $' + beer.price + '</div>' + 
+    '<div id="cart"> <a href="../Detail%20page/index.html"> Buy</a> </div>' + 
+    '</div>';
+    console.log(s);
+    $('#main-right').append(s);
+}
+
+function handleException(request, message, error) {
+    var msg = "";
+    msg += "Code: " + request.status + "\n";
+    msg += "Text: " + request.statusText + "\n";
+    if (request.responseJSON != null) {
+        msg += "Message" +
+            request.responseJSON.Message + "\n";
+    }
+    alert(msg);
+}
